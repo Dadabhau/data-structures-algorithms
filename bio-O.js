56adb23e945c24c3a72de3c0f72c8db3c835871b
dadabhau <dadasthete987@gmail.com>
2023-06-18 11:48:43 +0530

    Big O Rules

--- bio-O.js
+++ bio-O.js
@@ -34,9 +34,10 @@ function funChallenge(input) {
     }
     return a; // O(1)
 }
-funChallenge()
+// funChallenge()
 // BIG O(3 + 4n)
 
+// Challenge
 // What is the Big O of the below function? (Hint, you may want to go line by line)
 function anotherFunChallenge(input) {
     let a = 5; // O(1)
@@ -55,3 +56,126 @@ function anotherFunChallenge(input) {
     let whoAmI = "I don't know"; // O(1)
 }
 // BIG O(4 + 5n)
+
+
+// BIG O RULE
+// 1. Worst Case
+// 2. Remove Constants
+// 3. Diifferent terms for inputs
+// 4. Drop Non Dominants
+
+// 1. Worst Case
+const nemo1 = ['nemo'];
+const everyone1 = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+function findNemo1(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log('Running')
+        if (array[i] === 'nemo') {
+            console.log('Found NEMO!');
+            break;
+            // But if nemo will last in array this will be worst case
+        }
+    }
+}
+
+findNemo1(everyone1);
+
+// 2. Remove Constants (Drop the Constant)
+function printFirstItemThenFirstHalfThenSayHi100Times(items) {
+    console.log(items[0]); // O(1)
+
+    var middleIndex = Math.floor(items.length / 2);
+    var index = 0;
+
+    // Half 
+    while (index < middleIndex) {
+        console.log(items[index]);
+        index++;
+    }
+    // 100 
+    for (var i = 0; i < 100; i++) {
+        console.log('hi');
+    }
+}
+// BIG O(1 + n/2 + 100) ignoring variable assignments and small calculations for this example
+// O(n/2 + 101)
+// O(n + 1)
+// O(n)
+function compressBoxesTwice(boxes) {
+    boxes.forEach(function (boxes) {
+        console.log(boxes)
+    });
+    boxes.froEach(function (boxes) {
+        console.log(boxes);
+    })
+}
+// O(a + b) that means O(2n)
+// Drop the constants
+// O(n)
+
+// 3. Diifferent terms for inputs
+function compressBoxesTwice(boxes, boxes2) {
+    boxes.forEach(function (boxes) {
+        console.log(boxes)
+    });
+    boxes2.froEach(function (boxes2) {
+        console.log(boxes);
+    })
+}
+// BIG O(a + b)
+
+// 
+//Log all pairs of array
+const boxesletter = ['a', 'b', 'c', 'd', 'e'];
+function logAllPairesOfArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            //console.log(array[i], array[j])
+            if (array[i] === array[j]) {
+                console.log('Hello this is matched to ' + array[i], array[j])
+            }
+            // else {
+            //     console.log('Not MAtched!')
+            // }
+        }
+    }
+}
+logAllPairesOfArray(boxesletter)
+
+// WITH FOREACH 
+function logAllPairesOfBoxes(boxes) {
+    boxes.forEach(function (firstBox) {
+        boxes.forEach(function (secondBox) {
+            console.log(firstBox, secondBox);
+            console.log('Second ForEAch')
+        })
+
+    })
+}
+logAllPairesOfBoxes(boxesletter)
+// Nested Loop that means instead of having to for loops one after another where we use addition, when you
+// see loops that are nested, we use multiplication.
+// BIG O(n * n) that is O(n ^ 2)
+
+// 4. Drop Non Dominants
+const num = [1, 2, 3, 4, 5]
+function printAllNumbersThenAllPairSums(numbers) {
+
+    console.log('these are the numbers:');
+    numbers.forEach(function (number) {
+        console.log(number);
+    });
+
+    console.log('and these are their sums:');
+    numbers.forEach(function (firstNumber) {
+        numbers.forEach(function (secondNumber) {
+            console.log(firstNumber + secondNumber);
+        });
+    });
+}
+printAllNumbersThenAllPairSums(num)
+// BIG O(n + n^2)
+// with drop non dominants this is O(n^2)
+// Exp. O(x^2+3x+100+x/2)
+// X = 5 O(5*2+15+100+250)
+// 0(n^2)
